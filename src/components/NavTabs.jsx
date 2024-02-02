11aca87fa19b028b5ee89cfcfddaba5a712f243a
Amy Lloyd <amylloyd30@gmail.com>
2024-02-02 14:05:20 +1030

    render pages with page change RUNNING

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,7 +1,7 @@
 function NavTabs({ currentPage, handlePageChange }) {
     return (
         <ul className="nav nav-tabs">
-            <li classNamte="nav-item">
+            <li className="nav-item">
                 <a href="#about"
                 onClick={() => handlePageChange('About')}
                 className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
--- src/components/PortfolioContainer.jsx
+++ src/components/PortfolioContainer.jsx
@@ -3,14 +3,18 @@ import NavTabs from './NavTabs';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
     if (currentPage === 'About') {
       return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+        return <Portfolio />;
     }
     if (currentPage === 'Contact') {
       return <Contact />;
