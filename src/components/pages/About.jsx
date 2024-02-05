64300c345410cbadd877f24ac99d274f2f454354
Amy Lloyd <amylloyd30@gmail.com>
2024-02-05 15:10:06 +1030

    update

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -3,7 +3,7 @@ export default function About() {
       <div className="row">
         <div className="column">
           <h1>About Me</h1>
-          <img src='./assets/headshot-filmstyle.jpg'/>
+          <img src=''/>
           <p>
               Amy Lloyd has a developed a wealth of skills in a variety of areas that contribute to the value of her work. She enjoys tackling problem solving, both alone and in a team. She is a great team player who asks many questions and spends time considering the ideas of others. 
           </p>
