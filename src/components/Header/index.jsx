e71a3cdc16c650e5e72ff691898c5df5a160c127
Amy Lloyd <amylloyd30@gmail.com>
2024-04-10 17:26:35 +0930

    refine header styling

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,7 +5,7 @@ export default function Header({}) {
     return (
       <div className="header image ">
               <Nav />
-              <h1 className="h1 ps-4 text-center ">Amy Lloyd</h1>
+              <h1 className="h1">Amy Lloyd</h1>
       </div>
     );
 }
