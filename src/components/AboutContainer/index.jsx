8a43bcca79f15f0d9ef2b5cc3f52271ce782598f
Amy Lloyd <amylloyd30@gmail.com>
2024-04-12 13:11:46 +0930

    add colour palette

--- src/components/AboutContainer/index.jsx
+++ src/components/AboutContainer/index.jsx
@@ -14,8 +14,8 @@ function AboutContainer() {
             <img className="css-img mx-auto" src="/assets/headshot-filmstyle.jpg" alt="Headshot of 40 year old woman with brown hair and subtly smiling at the camera" />
         </Col> */}
       
-        <Col md={8} className="ps-5 pe-2">
-            <div className='mt-3 pt-5 ms-3 ps-5 pe-3'>
+        <Col md={7} className="ps-5 pe-2">
+            <div className='mt-5 pt-5 ms-3 ps-5 pe-3'>
             <h1>Hi! I'm Amy</h1>
                 <p className='mt-1'> And I'm a junior full-stack developer with fast-growing skills</p>
                 <p> <span className="word-emphasis">Creator</span> who enjoys being involved in a project from its inception to completion</p>
@@ -23,12 +23,17 @@ function AboutContainer() {
                 <p> <span className="word-emphasis">Team player</span> who communicates well with colleagues and clients.</p>
             </div>
         </Col> 
-        <Col md={4} >
+        <Col md={4} className='me-5 mt-5 pt-5' >
             <div>
-                <img src='./assets/Connection_square.png' alt='a visual showing green and blue connecting lines on a dark grey background' width="200" height="200"/>
-                <button className='css-button'>
-                    <Link to="/portfolio">Check out my portfolio page!</Link>
-                </button>
+                <div>
+                    <img src='./assets/Connection_square.png' alt='a visual showing green and blue connecting lines on a dark grey background' width="200" height="200"/>
+                </div>
+                <div className='mt-3'>
+                    <button className='css-button'>
+                        <Link to="/portfolio">Check out my portfolio page!</Link>
+                    </button>
+                </div>
+
             </div>
         </Col>
       </Row>
